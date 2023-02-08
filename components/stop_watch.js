dc989e7ca4f48d01ee041028bb5e53c9e6813dae
Grayash <k_2684@naver.com>
2023-02-09 01:10:04 +0900

    feat: react-timer-hook 라이브러리 추가

--- components/stop_watch.js
+++ components/stop_watch.js
@@ -1,37 +1,23 @@
-import { useEffect, useState } from "react";
+import React, { useEffect } from "react";
+import { useStopwatch } from "react-timer-hook";
 
-export default function Stopwatch({ start }) {
-  const [time, setTime] = useState(0);
-  const [running, setRunning] = useState(false);
-
-  useEffect(() => {
-    setRunning(start === "/lecture");
-  }, [start]);
+export default function Stopwatch({ start_stop }) {
+  const { seconds, minutes, start, pause, reset } = useStopwatch({
+    autoStart: false,
+  });
 
   useEffect(() => {
-    let interval;
-    if (running) {
-      interval = setInterval(() => {
-        setTime((prevTime) => prevTime + 10);
-      }, 10);
-    } else if (!running) {
-      clearInterval(interval);
+    if (start_stop === "/lecture") {
+      start();
     }
-    return () => clearInterval(interval);
-  }, [running]);
+  }, [start_stop]);
+
   return (
     <div className="stopwatch">
       <div className="numbers">
-        <span>{("0" + Math.floor((time / 60000) % 60)).slice(-2)}:</span>
-        <span>{("0" + Math.floor((time / 1000) % 60)).slice(-2)}:</span>
-        <span>{("0" + ((time / 10) % 100)).slice(-2)}</span>
+        <span>{minutes}</span>분<span>{seconds}초</span>
       </div>
-      <div className="buttons">
-        <p>수강신청 진입 버튼 클릭시 자동 시작됩니다.</p>
-        {/* <button onClick={() => setRunning(true)}>Start</button>
-        <button onClick={() => setRunning(false)}>Stop</button>
-        <button onClick={() => setTime(0)}>Reset</button> */}
-      </div>
+      <p>수강신청 진입 버튼 클릭시 자동 시작됩니다.</p>
     </div>
   );
 }
