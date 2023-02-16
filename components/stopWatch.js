d1c0dca0d8b7e01c77b6cbd03e4a05d20ce3a0c5
Grayash <k_2684@naver.com>
2023-02-16 15:29:57 +0900

    chore: stop_watch -> stopWatch 이름 변경

--- components/stopWatch.js
+++ components/stopWatch.js
@@ -1,7 +1,11 @@
 import React, { useEffect } from "react";
 import { useStopwatch } from "react-timer-hook";
+import { useRecoilValue } from "recoil";
+import { stopWatchState } from "@/states/stopWatchState";
 
 export default function Stopwatch({ start_stop }) {
+  const stopState = useRecoilValue(stopWatchState);
+
   const { seconds, minutes, start, pause, reset } = useStopwatch({
     autoStart: false,
   });
@@ -18,6 +22,17 @@ export default function Stopwatch({ start_stop }) {
         <span>{minutes}</span>분<span>{seconds}초</span>
       </div>
       <p>수강신청 진입 버튼 클릭시 자동 시작됩니다.</p>
+      <div className="btn_wrapper">
+        <button className="timer_btn" onClick={start}>
+          시작
+        </button>
+        <button className="timer_btn" onClick={pause}>
+          일시정지
+        </button>
+        <button className="timer_btn" onClick={reset}>
+          초기화
+        </button>
+      </div>
     </div>
   );
 }
